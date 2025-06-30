6ea08aa90575044f632c5bdcae88577b6fcf4596
218R1A0567 <akshithraj5003@gmail.com>
2025-06-30 11:19:47 +0530

    fix: attach Bootstrap to window to ensure modal works in production

--- Tetris_Game/src/main.jsx
+++ Tetris_Game/src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap; // ✅ Attach it manually
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
