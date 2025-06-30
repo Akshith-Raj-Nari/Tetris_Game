1350065c0d5d270a7747f9229917a231158ead56
218R1A0567 <akshithraj5003@gmail.com>
2025-06-30 16:38:40 +0530

    cors updated to accept all origins

--- server/score_board_service/app.js
+++ server/score_board_service/app.js
@@ -18,22 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Enable CORS for the specified origin
 // This allows the frontend to make requests to the backend
 // from http://localhost:5173 with credentials (cookies, authorization headers, etc.)
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://your-netlify-site.netlify.app", // Replace with your real Netlify URL
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("CORS not allowed for this origin"));
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // Use the imported routes
 app.use("/fetchscore", fetchScoreRouter);
